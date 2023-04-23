c0a1e61c4e141ad8186eaef24b005424de8e6bb5
Senior Fullstack Dev <13021427+nguyenvanhuan243@users.noreply.github.com>
2023-04-23 10:34:13 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import Example from "./Example";
+
+const App = () => <Example />;
+
+render(<App />, document.getElementById("root"));
